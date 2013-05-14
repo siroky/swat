a4989f4286382cdb782b695061c698f8b3edb808
siroky <siroky.honza@gmail.com>
2013-05-14 04:44:20 +0200

    Server-side JSON deserializer progress.

--- swat/runtime/common/src/main/scala/swat/common/json/JsonSerializer.scala
+++ swat/runtime/common/src/main/scala/swat/common/json/JsonSerializer.scala
@@ -3,28 +3,28 @@ package swat.common.json
 import play.api.libs.json._
 import scala.reflect.runtime.universe._
 import scala.collection.mutable
-import scala.runtime.BoxedUnit
+import scala.reflect.internal.MissingRequirementError
 
 /**
  * A custom JSON object serializer and deserializer. The JSON always has the same structure no matter what actually
- * the serialized object is. The root JSON node is an object with fields "value" which contains the object that is
- * being serialized and "objects" which contains all instances in the serialization object graph. An instance has
+ * the serialized object is. The root JSON node is an object with fields "$value" which contains the object that is
+ * being serialized and "$objects" which contains all instances in the serialization object graph. An instance has
  * it's id (field "$id") and type (field "$type") specified. The rest of the fields are the vals and vars. AnyVals are
  * serialized to JSON primitive types, Arrays to JSON arrays, AnyRefs to instances in the repository and singleton
  * objects to references. An example covering all the concepts follows:
  *
  * {{{
  * {
- *     value: { ref: 0 }, // The root object.
- *     objects: [ // Repository of all AnyRef objects.
+ *     $value: { $ref: 0 }, // The root object.
+ *     $objects: [ // Repository of all AnyRef objects.
  *         {
  *             $id: 0, // Id of the object, which is used while referencing it.
  *             $type: "my.package.X", // Type of the instance.
  *             a: "Foo", // AnyVal.
- *             b: { ref: 1 }, // Reference to an object.
- *             c: { ref: "scala.collection.immutable.Nil", // Reference to a singleton object.
+ *             b: { $ref: 1 }, // Reference to an object.
+ *             c: { $ref: "scala.collection.immutable.Nil", // Reference to a singleton object.
  *             d: [ 123, 456, 789 ], // Array of AnyVals.
- *             e: [ { ref: 1 }, { ref: 2 } ] // Array of AnyRefs.
+ *             e: [ { $ref: 1 }, { $ref: 2 } ] // Array of AnyRefs.
  *         },
  *         {
  *             $id: 1,
@@ -43,6 +43,12 @@ import scala.runtime.BoxedUnit
 @swat.ignored
 class JsonSerializer(val mirror: Mirror = runtimeMirror(getClass.getClassLoader)) {
 
+    val valueField = "$value"
+    val objectsField = "$objects"
+    val idField = "$id"
+    val typeField = "$type"
+    val refField = "$ref"
+
     /**
      * Serializes the specified object to a JSON string that conforms to the specification described at
      * [[swat.common.json.JsonSerializer]].
@@ -52,7 +58,7 @@ class JsonSerializer(val mirror: Mirror = runtimeMirror(getClass.getClassLoader)
         val objectIds = mutable.HashMap[AnyRef, Int]()
         val serializedObjects = mutable.ListBuffer[JsObject]()
 
-        // Internal serialization function.
+        /** Serializes the specified value to either a primitive value, array or reference. */
         def serializeValue(value: Any): JsValue = value match {
             case null | () => JsNull
             case b: Boolean => JsBoolean(b)
@@ -69,7 +75,10 @@ class JsonSerializer(val mirror: Mirror = runtimeMirror(getClass.getClassLoader)
             case x => throw new JsonException(s"Can't serialize value '$x'.")
         }
 
-        // Internal serialization of AnyRefs.
+        /**
+         * If the object hasn't been serialized yet, serializes it to JSON and adds it to the serializedObjects with
+         * a new id. Returns reference to the object. A singleton object is serialized as reference to it's type name.
+         */
         def serializeAnyRef(obj: AnyRef): JsObject = {
             val reference = objectIds.get(obj).map(JsNumber(_)).getOrElse {
                 // The object hasn't been serialized yet.
@@ -82,35 +91,29 @@ class JsonSerializer(val mirror: Mirror = runtimeMirror(getClass.getClassLoader)
                     val id = objectId
                     objectIds += obj -> id
                     objectId += 1
-                    serializedObjects += serializeObject(obj, id, symbol)
+
+                    val members = symbol.typeSignature.members
+                    val getters = members.collect { case m: MethodSymbol if m.isGetter => m }
+                    val serializedFields = getters.map { getter =>
+                        val objMirror = mirror.reflect(obj)
+                        val getterMirror = objMirror.reflectMethod(getter)
+                        val value = getterMirror.apply()
+                        getter.name.toString -> serializeValue(value)
+                    }
+                    val internalFields = Seq(idField -> JsNumber(id), typeField -> JsString(symbol.fullName))
+
+                    serializedObjects += JsObject(internalFields ++ serializedFields)
                     JsNumber(id)
                 }
             }
 
-            JsObject(Seq("ref" -> reference))
+            JsObject(Seq(refField -> reference))
         }
 
-        // Internal serialization of objects using reflection.
-        def serializeObject(obj: AnyRef, id: Int, symbol: Symbol): JsObject = {
-            // Serialize all fields with getters.
-            val members = symbol.typeSignature.members
-            val getters = members.collect { case m: MethodSymbol if m.isGetter => m }
-            val serializedFields = getters.map { getter =>
-                val objMirror = mirror.reflect(obj)
-                val getterMirror = objMirror.reflectMethod(getter)
-                val value = getterMirror.apply()
-                getter.name.toString -> serializeValue(value)
-            }
-
-            JsObject(Seq(
-                "$id" -> JsNumber(id),
-                "$type" -> JsString(symbol.fullName)
-            ) ++ serializedFields)
-        }
-
+        // Just start serializing the root object. All referenced objects get discovered during the process.
         Json.prettyPrint(JsObject(Seq(
-            "value" -> serializeValue(obj),
-            "objects" -> JsArray(serializedObjects)
+            valueField -> serializeValue(obj),
+            objectsField -> JsArray(serializedObjects)
         )))
     }
 
@@ -122,95 +125,115 @@ class JsonSerializer(val mirror: Mirror = runtimeMirror(getClass.getClassLoader)
      */
     def deserialize(json: String, tpe: Option[Type] = None): Any = {
         val deserializedObjects = mutable.HashMap[Int, Any]()
-        val referenceResolvers = mutable.ListBuffer[() => Unit]()
+        val referencesToResolve = mutable.ListBuffer[Reference]()
 
-        def deserializeValue(value: JsValue, tpe: Option[Type]): Any = value match {
-            case JsNull => convertValue(null, tpe)
-            case JsBoolean(b) => convertValue(b, tpe)
-            case JsNumber(n) => convertValue(n, tpe)
-            case JsString(s) => convertValue(s, tpe)
-            case JsArray(items) => deserializeArray(items, tpe)
-            case JsObject(Seq(("ref", JsNumber(id)))) => deserializedObjects.get(id.toInt) match {
-                case Some(deserializedObject) => convert(deserializedObject, tpe)
-                case None => Reference(id.toInt)
-            }
+        /**
+         * Deserializes the specified value. Primitive types, arrays, references to singleton objects and references
+         * to already deserialized objects are immediately returned.
+         */
+        def deserializeValue(value: JsValue, tpe: Option[Type]): Either[Int, Any] = value match {
+            case JsNull => Right(convert(null, tpe))
+            case JsBoolean(b) => Right(convert(b, tpe))
+            case JsNumber(n) => Right(convert(n, tpe))
+            case JsString(s) => Right(convert(s, tpe))
+            case JsArray(items) => Right(deserializeArray(items, tpe))
+            case JsObject(Seq((r, reference))) if r == refField => deserializeReference(reference, tpe)
             case _ => throw new JsonException(s"Cannot deserialize unrecognized value '$value'.")
         }
 
-        def deserializeArray(items: Seq[JsValue], tpe: Option[Type]): Any = {
-            val itemTpe = tpe.map(t => t.t)
-            val (array, itemTpe) = tpe match {
-                case Some(arrayType) => {
-
-                }
-                case None => (new Array[Any](items.length), None)
+        /** Deserializes an array. */
+        def deserializeArray(items: Seq[JsValue], tpe: Option[Type]): Array[Any] = {
+            val result = new Array[Any](items.length)
+            val itemTpe = tpe.filter(_ <:< typeOf[Array[_]]).map(t => t.asInstanceOf[TypeRefApi].args.head)
+            items.map(i => deserializeValue(i, itemTpe)).zipWithIndex.foreach {
+                case (Right(v), index) => result.update(index, v)
+                case (Left(id), index) => referencesToResolve += ArrayItemReference(result, index, id)
             }
-
-            tpe match {
-                case Some(tpe) =>
-
-                case None => {
-                    val result = new Array[Any](items.length)
-                    items.zipWithIndex
-                }
-            }
+            result
         }
 
-        def processValue(value: Any, tpe: Option[Type], valueSetter: Any => Unit) {
-            value match {
-                case Reference(id) => referenceResolvers += {
-                    deserializedObjects.get(id) match {
-                        case Some(obj) =>
-                        case None => throw new JsonException(s"Cannot resolver reference to object with id '$id'.")
+        /** Deserializes both references to other objects and references to singleton objects. */
+        def deserializeReference(reference: JsValue, tpe: Option[Type]): Either[Int, Any] = reference match {
+            case JsNumber(id) => deserializedObjects.get(id.toInt) match {
+                case Some(deserializedObject) => Right(convert(deserializedObject, tpe))
+                case None => Left(id.toInt)
+            }
+            case JsString(typeFullName) => Right(mirror.reflectModule(mirror.staticModule(typeFullName)).instance)
+            case _ => throw new JsonException(s"Unrecongized reference '$reference'.")
+        }
+
+        /** Deserializes the specified object and registers it to the deserializedObjects under its id. */
+        def deserializeObject(value: JsValue): Any = value match {
+            case JsObject(fields) => {
+                // TODO
+            }
+            case _ => throw new JsonException(s"Cannot deserialize object '$value'.")
+        }
+
+        /** Resolves all unresolved references. */
+        def resolveReferences() {
+            // TODO
+        }
+
+        /** Returns a deserialized object with the specified id. */
+        def dereference(id: Int): Any = deserializedObjects.get(id) match {
+            case Some(obj) => obj
+            case None => throw new JsonException(s"Cannot resolve reference to object with id '$id'.")
+        }
+
+        try {
+            Json.parse(json) match {
+                case JsObject(Seq((v, value), (o, objects: JsArray))) if v == valueField && o == objectsField => {
+                    // Deserialize all objects and resolve the unresolved references.
+                    objects.value.foreach(deserializeObject _)
+                    resolveReferences()
+
+                    // Return the root value.
+                    deserializeValue(value, tpe) match {
+                        case Right(x) => x
+                        case Left(id) => convert(dereference(id), tpe)
                     }
                 }
-                case v => valueSetter(v)
+                case _ => throw new JsonException(s"Cannot deserialize non-conforming JSON structure '$json'.")
             }
-        }
-
-        Json.parse(json) match {
-            case JsObject(Seq(("value", value), ("objects", objects: JsArray))) => {
-
-            }
-            case _ => throw new JsonException(s"Cannot deserialize non-conforming structure '$json'.")
+        } catch {
+            case e: MissingRequirementError => throw new JsonException(e.getMessage)
         }
     }
 
+    /**
+     * Converts the specified value to the target type if the conversion is possible. If the target type isn't defined,
+     * returns the input value. Otherwise returns the converted value.
+     */
     private def convert(value: Any, targetTpe: Option[Type]): Any = targetTpe match {
-        // The target type is specified so the value has to be converted if necessary.
         case Some(tpe) => value match {
             case null if tpe <:< typeOf[AnyRef] => null
             case null if tpe <:< typeOf[Unit] => ()
             case v if v != null && classToSymbol(v.getClass).typeSignature <:< tpe => v
-            case n: BigDecimal if tpe weak_<:< typeOf[Double] => numericConvertors(tpe)(n)
+            case n: BigDecimal if tpe weak_<:< typeOf[Double] => tpe match {
+                case t if t =:= typeOf[Byte] => n.toByte
+                case t if t =:= typeOf[Short] => n.toShort
+                case t if t =:= typeOf[Int] => n.toInt
+                case t if t =:= typeOf[Long] => n.toLong
+                case t if t =:= typeOf[Float] => n.toFloat
+                case t if t =:= typeOf[Double] => n.toDouble
+            }
             case s: String if tpe =:= typeOf[Char] && s.length == 1 => s.head
+            case v if tpe =:= typeOf[String] => v.toString
             case _ => throw new JsonException(s"Cannot convert '$value' to type '$tpe'.")
         }
-
-        // Target type isn't specified so the value may be anything.
         case None => value
     }
-
-    /** Convertors of BigDecimal to other numeric types indexed by the types. */
-    private val numericConvertors: Map[Type, BigDecimal => AnyVal](
-        typeOf[Byte] -> toByte,
-        typeOf[Short] -> toShort,
-        typeOf[Int] -> toInt,
-        typeOf[Long] -> toLong,
-        typeOf[Float] -> toFloat,
-        typeOf[Double] -> toDouble
-    )
-
-    /** Reference to an object with the specified id. */
-    private case class Reference(id: Int)
 
     /** Cache of class symbols. */
     private val classSymbols = mutable.HashMap[Class[_], Symbol]()
 
-    /**
-     * Returns symbol corresponding to the specified class utilizing the class symbol cache.
-     */
+    /** Returns symbol corresponding to the specified class utilizing the class symbol cache. */
     private def classToSymbol(c: Class[_]): Symbol = {
-        classSymbols.getOrElseUpdate(c, mirror.classSymbol(c.getClass))
+        classSymbols.getOrElseUpdate(c, mirror.classSymbol(c))
     }
+
+    private sealed abstract class Reference
+    private case class ObjectFieldReference(target: Any, field: TermSymbol, id: Int) extends Reference
+    private case class ArrayItemReference(target: Array[Any], index: Int, id: Int) extends Reference
 }
--- swat/runtime/common/src/test/scala/swat/common/JsonSerializerTests.scala
+++ swat/runtime/common/src/test/scala/swat/common/JsonSerializerTests.scala
@@ -2,46 +2,48 @@ package swat.common
 
 import org.scalatest.FunSuite
 import swat.common.json.JsonSerializer
+import scala.reflect.runtime.universe._
 
 class A(var a: A)
 
 class JsonSerializerTests extends FunSuite {
+    val mirror: Mirror = runtimeMirror(getClass.getClassLoader)
 
-    test("Primitive types are serialized to JSON primitive types.") {
-        () shouldSerializeToValue """null"""
-        (null: Any) shouldSerializeToValue """null"""
-        true shouldSerializeToValue """true"""
-        12.toByte shouldSerializeToValue """12"""
-        123.toShort shouldSerializeToValue """123"""
-        1234 shouldSerializeToValue """1234"""
-        12345.toLong shouldSerializeToValue """12345"""
-        1234.5.toFloat shouldSerializeToValue """1234.5"""
-        12345.6 shouldSerializeToValue """12345.6"""
-        'c' shouldSerializeToValue """"c""""
-        "foo" shouldSerializeToValue """"foo""""
+    test("Primitive values are serialized to JSON primitive values.") {
+        () shouldSerializeTo wrapValue("null")
+        (null: Any) shouldSerializeTo wrapValue("null")
+        true shouldSerializeTo wrapValue("true")
+        12.toByte shouldSerializeTo wrapValue("12")
+        123.toShort shouldSerializeTo wrapValue("123")
+        1234 shouldSerializeTo wrapValue("1234")
+        12345.toLong shouldSerializeTo wrapValue("12345")
+        1234.5.toFloat shouldSerializeTo wrapValue("1234.5")
+        12345.6 shouldSerializeTo wrapValue("12345.6")
+        'c' shouldSerializeTo wrapValue(""""c"""")
+        "foo" shouldSerializeTo wrapValue(""""foo"""")
     }
 
     test("Arrays are serialized to JSON arrays.") {
-        Array() shouldSerializeToValue """[ ]"""
-        Array(123, 456, 789) shouldSerializeToValue """[ 123, 456, 789 ]"""
-        Array("foo", "bar", "baz") shouldSerializeToValue """[ "foo", "bar", "baz" ]"""
+        Array[Any]() shouldSerializeTo wrapValue("[ ]")
+        Array(123, 456, 789) shouldSerializeTo wrapValue("[ 123, 456, 789 ]")
+        Array("foo", "bar", "baz") shouldSerializeTo wrapValue("""[ "foo", "bar", "baz" ]""")
     }
 
     test("Objects and singleton objects are serialized to references.") {
         List(Some(Some(true)), Right("foo"), 123, 456) shouldSerializeTo """
             {
-                "value":{"ref":0},
-                "objects": [
+                "$value":{"$ref":0},
+                "$objects": [
                     {
                         "$id":3,
                         "$type":"scala.collection.immutable.$colon$colon",
-                        "tl":{"ref":"scala.collection.immutable.Nil"},
+                        "tl":{"$ref":"scala.collection.immutable.Nil"},
                         "hd":456
                     },
                     {
                         "$id":2,
                         "$type":"scala.collection.immutable.$colon$colon",
-                        "tl":{"ref":3},
+                        "tl":{"$ref":3},
                         "hd":123
                     },
                     {
@@ -52,8 +54,8 @@ class JsonSerializerTests extends FunSuite {
                     {
                         "$id":1,
                         "$type":"scala.collection.immutable.$colon$colon",
-                        "tl":{"ref":2},
-                        "hd":{"ref":4}
+                        "tl":{"$ref":2},
+                        "hd":{"$ref":4}
                     },
                     {
                         "$id":6,
@@ -63,13 +65,13 @@ class JsonSerializerTests extends FunSuite {
                     {
                         "$id":5,
                         "$type":"scala.Some",
-                        "x":{"ref":6}
+                        "x":{"$ref":6}
                     },
                     {
                         "$id":0,
                         "$type":"scala.collection.immutable.$colon$colon",
-                        "tl":{"ref":1},
-                        "hd":{"ref":5}
+                        "tl":{"$ref":1},
+                        "hd":{"$ref":5}
                     }
                 ]
             }
@@ -84,32 +86,55 @@ class JsonSerializerTests extends FunSuite {
 
         a1 shouldSerializeTo """
             {
-                "value":{"ref":0},
-                "objects": [
+                "$value":{"$ref":0},
+                "$objects": [
                     {
                         "$id":2,
                         "$type":"swat.common.A",
-                        "a":{"ref":0}
+                        "a":{"$ref":0}
                     },
                     {
                         "$id":1,
                         "$type":"swat.common.A",
-                        "a":{"ref":2}
+                        "a":{"$ref":2}
                     },
                     {
                         "$id":0,
                         "$type":"swat.common.A",
-                        "a":{"ref":1}
+                        "a":{"$ref":1}
                     }
                 ]
             }
                              """
     }
 
+    test("Primitive values are deserialized from JSON primitive values.") {
+        wrapValue("null") shouldDeserializeTo(null)
+        wrapValue("true") shouldDeserializeTo(true)
+        wrapValue("12") shouldDeserializeTo(12.toByte, Some(typeOf[Byte]))
+        wrapValue("123") shouldDeserializeTo(123.toShort, Some(typeOf[Short]))
+        wrapValue("1234") shouldDeserializeTo(1234, Some(typeOf[Int]))
+        wrapValue("12345") shouldDeserializeTo(12345.toLong, Some(typeOf[Long]))
+        wrapValue("1234.5") shouldDeserializeTo(1234.5.toFloat, Some(typeOf[Float]))
+        wrapValue("12345.6") shouldDeserializeTo(12345.6, Some(typeOf[Double]))
+        wrapValue(""""c"""") shouldDeserializeTo('c', Some(typeOf[Char]))
+        wrapValue(""""foo"""") shouldDeserializeTo("foo", Some(typeOf[String]))
+    }
+
+    test("Arrays are deserialized from JSON primitive arrays.") {
+        wrapValue("[ ]") shouldDeserializeTo(Array[Any]())
+        wrapValue("[ 1, 2, 3 ]") shouldDeserializeTo(Array[Any](1, 2, 3))
+        wrapValue("[ 1, 2, 3 ]") shouldDeserializeTo(Array[Int](1, 2, 3), Some(typeOf[Array[Int]]))
+    }
+
+    test("Singleton objects are deserialized from references.") {
+        wrapValue("""{"$ref":"scala.collection.immutable.Nil"}""") shouldDeserializeTo(Nil)
+    }
+
     implicit class SerializableObject(obj: Any) {
         def shouldSerializeTo(json: String) {
             val expected = normalize(json)
-            val actual = normalize(new JsonSerializer().serialize(obj))
+            val actual = normalize(new JsonSerializer(mirror).serialize(obj))
             if (expected != actual) {
                 fail(
                     s"""|The serialized value isn't equal to the expected.
@@ -120,19 +145,19 @@ class JsonSerializerTests extends FunSuite {
             }
         }
 
-        def shouldSerializeToValue(value: String) {
-            shouldSerializeTo(s"""{ "value": $value, "objects": [ ] }""")
-        }
-
         def normalize(json: String): String = {
             json.lines.map(_.replace(" ", "")).filter(!_.isEmpty).mkString
         }
     }
 
     implicit class DeserializableString(json: String) {
-        def shouldDeserializeTo(expected: Any) {
-            val actual = new JsonSerializer().deserialize(json)
-            if (expected != actual) {
+        def shouldDeserializeTo(expected: Any, tpe: Option[Type] = None) {
+            val actual = new JsonSerializer(mirror).deserialize(json, tpe)
+            val succeeded = (expected, actual) match {
+                case (a: Array[_], b: Array[_]) if a.length == b.length => (0 until a.length).forall(i => a(i) == b(i))
+                case (a, b) => a == b
+            }
+            if (!succeeded) {
                 fail(
                     s"""|The deserialized object isn't equal to the expected.
                         |EXPECTED: '$expected'
@@ -142,6 +167,8 @@ class JsonSerializerTests extends FunSuite {
             }
         }
     }
+
+    def wrapValue(value: String): String = s"""{ "$$value": $value, "$$objects": [ ] }"""
 }
 
 
