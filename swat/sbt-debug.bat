406c81f61578f263309ed9c7a4dc02ff218b97bc
siroky <siroky.honza@gmail.com>
2013-07-28 03:43:26 +0200

    Implementation almost finished, User manual.

--- swat/sbt-debug.bat
+++
@@ -1,2 +0,0 @@
-set SCRIPT_DIR=%~dp0
-java -Xmx1024M -XX:MaxPermSize=512M -Xss2M -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5006 -jar "%SCRIPT_DIR%sbt-launch.jar" %*
---
+++ swat/sbt.sh
@@ -0,0 +1,1 @@
+java -Xmx512M -XX:MaxPermSize=512M -Xss2M -jar `dirname $0`/sbt-launch.jar "$@"
